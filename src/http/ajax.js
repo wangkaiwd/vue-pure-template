c6168c441ebc18c34ac2f7cc16234767a0cc549a
wangkaiwd <1162486176@qq.com>
2019-03-01 17:32:44 +0800

    feat: post,get请求，请求头：application/json和application/x-www-form-urlencoded请求处理

--- src/http/ajax.js
+++ src/http/ajax.js
@@ -1,10 +1,22 @@
 import axiosInstance from './axios';
-
-const ajax = (url, method = 'post') => {
-  return (params) => {
-    const paramsKey = method === 'post' ? 'data' : 'params';
-    return axiosInstance({ method, url, [paramsKey]: params });
+import qs from 'qs';
+// qs: 将js对象转换为 key1=val1&key2=val2格式的工具
+const ajax = (url, method = 'post') => (params) => {
+  const cfg = {
+    post: {
+      // headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      data: qs.stringify(params)
+    },
+    get: { params }
   };
+  // 请求头默认都是 Content-Type: application/json
+  return axiosInstance(
+    {
+      method,
+      url,
+      ...cfg[method]
+    }
+  );
 };
 
 export default ajax;

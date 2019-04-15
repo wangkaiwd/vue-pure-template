a9c5fa427dde19569faf10c53d50674c4725ac56
wangkaiwd <1162486176@qq.com>
2019-04-15 11:05:08 +0800

    fix: 配置侧边栏后重新部署

---
+++ dist_test/precache-manifest.4cf01265bb18c35e6981f6e9a7d0a105.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "79f7e85f87f91f13ee5e",
+    "url": "/vue-cli3.0-template/dist_test/css/chunk-4d15be60.e083ca98.css"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/vue-cli3.0-template/dist_test/robots.txt"
+  },
+  {
+    "revision": "79f7e85f87f91f13ee5e",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-4d15be60.f1312299.js"
+  },
+  {
+    "revision": "dc1c308d85448f026922",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-2d0d7635.867cc5eb.js"
+  },
+  {
+    "revision": "39d9b5155f4c2f03d47a",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-2d0e5f03.1297f211.js"
+  },
+  {
+    "revision": "265fd097b45947190af1",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-2d22d815.aad5ffda.js"
+  },
+  {
+    "revision": "d7e6c705d8b51b325a59",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-vendors.5de50d48.js"
+  },
+  {
+    "revision": "2d72bc93c6f718178616",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-4153a8c6.5a8b8b4a.js"
+  },
+  {
+    "revision": "0bc831b480635be19194",
+    "url": "/vue-cli3.0-template/dist_test/js/chunk-2d0e19c3.c4a0877e.js"
+  },
+  {
+    "revision": "5d9cae68f72bf95b0e12",
+    "url": "/vue-cli3.0-template/dist_test/js/app.ac5add31.js"
+  },
+  {
+    "revision": "eb4a40d923c1e1ba263831cb4ff8eeea",
+    "url": "/vue-cli3.0-template/dist_test/index.html"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "/vue-cli3.0-template/dist_test/fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "/vue-cli3.0-template/dist_test/fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "d7e6c705d8b51b325a59",
+    "url": "/vue-cli3.0-template/dist_test/css/chunk-vendors.ef2a2ae9.css"
+  },
+  {
+    "revision": "2d72bc93c6f718178616",
+    "url": "/vue-cli3.0-template/dist_test/css/chunk-4153a8c6.68a77357.css"
+  },
+  {
+    "revision": "5d9cae68f72bf95b0e12",
+    "url": "/vue-cli3.0-template/dist_test/css/app.6107d764.css"
+  }
+];
